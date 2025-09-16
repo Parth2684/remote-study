3004fd92770d07a055d30e1bf78b7d3f98bed586
Parth2684 <bhosle6006@gmail.com>
2025-09-16 21:45:15 +0530

    corrected singleton

--- client/app/api/(auth)/signup/route.ts
+++ client/app/api/(auth)/signup/route.ts
@@ -1,7 +1,27 @@
-import { NextRequest } from "next/server";
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
 
 
+const signupSchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string(),
+    role: z.enum(["STUDENT", "INSTRUCTOR"])
+})
 
 export const POST = async (req: NextRequest) => {
-    
+    const body = await req.json();
+    const parsedBody = signupSchema.safeParse(body)
+    if(!parsedBody.success) {
+        return NextResponse.json({
+            message: "Incorrect or Incomplete details"
+        }, {
+            status: 411
+        })
+    }
+    const role = (parsedBody.data.role == "INSTRUCTOR") ? "instructor" : "student";
+    if(role == "instructor") {
+        await prisma
+    }
 } 
