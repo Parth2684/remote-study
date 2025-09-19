b793ebd4143cd15c6e0feda99dce848b1625eb93
Parth2684 <bhosle6006@gmail.com>
2025-09-20 00:19:26 +0530

    added favicon and uncommented the code

--- client/app/api/instructor/classroom/[classroomId]/create-quiz/route.ts
+++ client/app/api/instructor/classroom/[classroomId]/create-quiz/route.ts
@@ -41,50 +41,50 @@ export async function POST(req: NextRequest,  { params }: { params: Promise<{ cl
         const { title, description, questionAnswer } = parsedBody.data
         const classroomId = (await params).classroomId
     
-        // await prisma.$transaction(async (tx) => {
-        //     const quizId = uuidv4()
-        //     await tx.quiz.create({
-        //         data: {
-        //             id: quizId,
-        //             instructorId: instructor.id,
-        //             title,
-        //             description,
-        //             classroomId: classroomId
-        //         }
-        //     })
+        await prisma.$transaction(async (tx) => {
+            const quizId = uuidv4()
+            await tx.quiz.create({
+                data: {
+                    id: quizId,
+                    instructorId: instructor.id,
+                    title,
+                    description,
+                    classroomId: classroomId
+                }
+            })
     
-        //     const { questionArray, optionArray } = questionAnswer.reduce<{ questionArray: { id: string; text: string; quizId: string }[], optionArray: { id: string; text: string; isCorrect: boolean; questionId: string }[] }>(
-        //         (acc, questionAnswerPair) => {
-        //             const id = uuidv4();
+            const { questionArray, optionArray } = questionAnswer.reduce<{ questionArray: { id: string; text: string; quizId: string }[], optionArray: { id: string; text: string; isCorrect: boolean; questionId: string }[] }>(
+                (acc, questionAnswerPair) => {
+                    const id = uuidv4();
                 
-        //             const options = questionAnswerPair.options.map((option) => ({
-        //             id: uuidv4(),
-        //             text: option,
-        //             isCorrect: option === questionAnswerPair.correctOption,
-        //             questionId: id,
-        //             }));
+                    const options = questionAnswerPair.options.map((option) => ({
+                    id: uuidv4(),
+                    text: option,
+                    isCorrect: option === questionAnswerPair.correctOption,
+                    questionId: id,
+                    }));
                 
-        //             acc.questionArray.push({
-        //             id,
-        //             text: questionAnswerPair.question,
-        //             quizId,
-        //             });
+                    acc.questionArray.push({
+                    id,
+                    text: questionAnswerPair.question,
+                    quizId,
+                    });
                 
-        //             acc.optionArray.push(...options);
+                    acc.optionArray.push(...options);
                 
-        //             return acc;
-        //         },
-        //         { questionArray: [], optionArray: [] }
-        //     );
-        //     await Promise.all([
-        //         tx.question.createMany({
-        //             data: questionArray
-        //         }),
-        //         tx.option.createMany({
-        //             data: optionArray
-        //         })
-        //     ])
-        // })
+                    return acc;
+                },
+                { questionArray: [], optionArray: [] }
+            );
+            await Promise.all([
+                tx.question.createMany({
+                    data: questionArray
+                }),
+                tx.option.createMany({
+                    data: optionArray
+                })
+            ])
+        })
     
         return NextResponse.json({
             message: "Quiz Created"
