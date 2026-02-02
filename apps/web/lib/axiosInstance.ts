7b34d15db5285832204a3443919290c5a6b56433
Parth2684 <bhosle6006@gmail.com>
2026-02-02 11:12:41 +0530

    remove backend from next api and from .env.example

--- apps/web/lib/axiosInstance.ts
+++ apps/web/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const HTTP_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`
+export const HTTP_URL = `${process.env.BACKEND_URL}/api`
 
 export const axiosInstance = axios.create({
     baseURL: HTTP_URL,
