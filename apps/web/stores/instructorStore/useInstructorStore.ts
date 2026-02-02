11f6531dd728f4c49508c9edf625c26c65531e1c
Parth2684 <bhosle6006@gmail.com>
2026-02-02 12:59:32 +0530

    add instructor router

--- apps/web/stores/instructorStore/useInstructorStore.ts
+++ apps/web/stores/instructorStore/useInstructorStore.ts
@@ -1,25 +1,32 @@
-import { create } from "zustand"
-import { InstructorAction, InstructorState } from './types'
-import { axiosInstance } from '../../lib/axiosInstance'
-import toast from 'react-hot-toast'
+import { create } from "zustand";
+import { InstructorAction, InstructorState } from "./types";
+import { axiosInstance } from "../../lib/axiosInstance";
+import toast from "react-hot-toast";
 
+export const useInstructorStore = create<InstructorState & InstructorAction>((set, get) => ({
+  classrooms: [],
 
-export const useInstructorStore = create<InstructorState & InstructorAction>((set, get) => ({ 
-  classrooms: null,
-  
+  getClassrooms: async () => {
+    try {
+      const res = await axiosInstance.get("/instructor/classroom/my-classrooms");
+      set({ classrooms: res.data.classrooms });
+    } catch (error) {
+      console.error(error);
+      toast.error("error fetching classrooms");
+    }
+  },
   createClassroom: async (name: string, description: string) => {
     try {
       const res = await axiosInstance.post("/instructor/classroom/create-classroom", {
         name,
-        description
-      })
-      set({classrooms: ...res.})
-      
+        description,
+      });
+      set((state) => ({
+        classrooms: [...state.classrooms, res.data.classrooms],
+      }));
     } catch (error) {
-      console.error(error)
-      toast.error("Error creating classroom")
+      console.error(error);
+      toast.error("Error creating classroom");
     }
-    
-    
-  }
-}))
+  },
+}));
