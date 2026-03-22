73dd298d61b6f0586b6d7257323fc92016635cfc
Parth2684 <bhosle6006@gmail.com>
2026-03-22 21:05:59 +0530

    fix stream and websocket

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTION"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie", "Origin"],
     exposedHeaders: ["Set-Cookie"],
     preflightContinue: false,
